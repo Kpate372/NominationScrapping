af288f901e82cb563f6a9b72d5e7b7c36241e2af
Kpate372 <kpate372@uwo.ca>
2020-03-04 22:02:38 -0500

    added logic for finding update of today

--- NominationScrapping/NominationScrapping/spiders/pnpSpider.py
+++ NominationScrapping/NominationScrapping/spiders/pnpSpider.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import datetime
 
 class PnpspiderSpider(scrapy.Spider):
     name = 'pnpSpider'
     allowed_domains = ['api.ontario.ca']
-    start_urls = ['http://api.ontario.ca/']
+    start_urls = ['https://api.ontario.ca/api/drupal/page%2F2020-ontario-immigrant-nominee-program-updates?fields=nid,field_body_beta,body']
 
     def parse(self, response):
-        pass
+        todayDate = datetime.datetime.now()
+        dateString = todayDate.strftime("%B %-d, %Y")
+        match = data.find(dateString)
+        if match > -1:
+            print("Found an Update Today!!!!")
